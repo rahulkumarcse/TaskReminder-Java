9ba105feef39daf333dd7b5bea4c7d81649d62fe
Rahul Kumar <rahulcsofficial@gmail.com>
2020-10-25 21:03:27 +0530

    First Commit

---
+++ TaskReminder/src/login/LoginCredentials.java
@@ -0,0 +1,12 @@
+package login;
+
+public class LoginCredentials {
+	String username;
+	String password;
+	public LoginCredentials(String username, String password) {
+		this.username= username;
+		this.password = password;
+		
+	}
+
+}
