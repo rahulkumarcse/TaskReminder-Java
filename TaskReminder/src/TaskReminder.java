4687774b50636cc3d9c488fa17c45753391edff5
Rahul Kumar <rahulcsofficial@gmail.com>
2020-10-31 19:56:29 +0530

    JDBC Working - Database Working

--- TaskReminder/src/TaskReminder.java
+++ TaskReminder/src/TaskReminder.java
@@ -1,8 +1,23 @@
-
+import login.LoginCredentials;
+import java.util.*;
 public class TaskReminder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the username");
+		String user = sc.next();
+		System.out.println("Enter your password");
+		String pass = sc.next();
+		sc.close();
+		LoginCredentials login1 =  new LoginCredentials(user,pass);
+		if(login1.dbCheck() == 1)
+		{
+			System.out.println("Executed Sucessfully");
+		}
+		else
+		{
+			System.out.println("Not executed");
+		}
 
 	}
 
--- TaskReminder/src/login/LoginCredentials.java
+++ TaskReminder/src/login/LoginCredentials.java
@@ -1,12 +1,32 @@
 package login;
-
+import java.sql.*;
 public class LoginCredentials {
-	String username;
-	String password;
+	 String username;
+	 String password;
 	public LoginCredentials(String username, String password) {
-		this.username= username;
-		this.password = password;
-		
+		 this.username= username;
+		this.password = password;	
+	
+	}
+	 public int dbCheck(){
+	
+	
+	try {
+		//Class.forName("com.mysql.jdbc.Driver");
+		Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/rahul",username,password);
+		Statement stmt = connect.createStatement();
+		ResultSet query = stmt.executeQuery("select * from rahul");
+		while(query.next()) {
+			System.out.println(query.getString("name"));
+		}
+		System.out.print(query);
+		connect.close();
+		return 1;
+	}catch(Exception e)
+	{
+		System.out.println(e);
+		return 0;
 	}
 
+}
 }
